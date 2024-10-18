75610e733042ea8d48e8821eeced79da7c1c8d2f
appu89 <reddyappu8888@gmail.com>
2024-10-18 03:41:59 -0400

    day 3

---
+++ src/python/var.py
@@ -0,0 +1,10 @@
+age=25
+height=5.5
+color="blue" 
+like_python = True
+print(f"Age: {age}, Height: {height}, Favorite color: {color}, Likes Python: {like_python}")
+age=26
+height=6.1
+color="pink" 
+like_python = False
+print(f"Age: {age}, Height: {height}, Favorite color: {color}, Likes Python: {like_python}")
